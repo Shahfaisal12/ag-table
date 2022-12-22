f3ac0e443a9eb9774e00b80e2b3e17b99a91df6c
shahfaisal12 <faisalmkd02@gmail.com>
2022-12-22 12:42:43 +0500

    'new'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Table from './Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
--- src/Table.js
+++ src/Table.js
@@ -27,25 +27,52 @@ const data = [
   { make: "Porsche", model: "Boxter", price: 72000 }
 ];
 
+const actionButton =(params) =>{
+    console.log(params);
+    alert(`${params.data.model} ${params.data.price}`);
+}
+
 const coloumns =[
     {
-        headerName:'Make', field:'make', checkboxSelection:true
+        headerName:'Make', field:'make', checkboxSelection:true, 
+        cellStyle:(params)=>(params.value === 'Toyota' ? {background:'pink'}: {background:'yellow'})
     },
     {
         headerName:'Model', field:'model'
     },
     {
-        headerName:'Price', field:'price'
+        headerName:'Price', field:'price', tooltipField:'make',
+        cellStyle:(params)=>(params.value === 35000 ? {background:'pink'}: {background:'yellow'})
+        // cellClass:(params)=>(params.value === 'Toyota' ? 'Green': 'Red')   /* red and green are class so we can style at in app.css */
     },
+    {
+        headerName:'Action', field:'price',
+         cellRendererFramework: (params)=> <div>
+         <button className='btn btn-success' onClick={()=>actionButton(params)}>clickme</button>
+         </div>
+    }
 ];
 
 const defaultColDef= {
-    sortable:true, editable:true, filter:true
+    sortable:true, editable:true, filter:true, floatingFilter:true, flex:1
+}
+
+let gridApi;
+
+const onGridReady = (params) =>{
+    gridApi=params.api
+}
+
+const onExportClick = () => {
+    gridApi.exportDataAsCsv();
 }
 
   return (
-    <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
-    <AgGridReact rowData={data} columnDefs={coloumns} defaultColDef={defaultColDef} />
+    <div className='container my-5'>
+    <button className='btn btn-outline-primary mb-5' onClick={()=>onExportClick()}>Download csv</button>
+    <div className="ag-theme-alpine" id='myGrid' style={{width: '100%', height: 500}}>
+    <AgGridReact rowData={data} columnDefs={coloumns} defaultColDef={defaultColDef} onGridReady={onGridReady}  enableBrowserTooltips={true} />
+    </div>
     </div>
   )
 }
